6d3c4fc522823b8fcdbdcb80916219b8bb1c531f
itdevteamcyberpunks <itdevteamcyberpunks@gmail.com>
2023-08-24 18:16:21 +0530

    added heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,9 @@ const IndexPage = ({data}) => (
     
       {data.allContentfulBand.edges.map(({ node }) => (
         
-        <div><h3>{node.name}</h3> 
+        <div>
+          <h1>Info about music bands</h1>
+          <h3>{node.name}</h3> 
         <p>{node.about.about}</p>
         <a href={node.website}>click here to listen to the music of {node.name}</a>
         </div>
